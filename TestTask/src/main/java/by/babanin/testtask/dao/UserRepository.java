219fe054710b29106c358e6516d27fcccdd68879
LordDetson <babanin.dima@gmail.com>
2019-07-04 13:26:30 +0300

    Added user repository with REST access.

--- TestTask/src/main/java/by/babanin/testtask/dao/UserRepository.java
+++ TestTask/src/main/java/by/babanin/testtask/dao/UserRepository.java
@@ -1,4 +1,9 @@
 package by.babanin.testtask.dao;
 
-public interface UserRepository {
+import by.babanin.testtask.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
+public interface UserRepository extends MongoRepository<User, String> {
 }
