b4e12f2253ca39975130c8f541d2c52588e0030e
LordDetson <babanin.dima@gmail.com>
2019-07-05 15:12:47 +0300

    User name conflict exception

--- TestTask/src/main/java/by/babanin/testtask/service/exception/UserExistsException.java
+++ TestTask/src/main/java/by/babanin/testtask/service/exception/UserExistsException.java
@@ -1,6 +1,10 @@
-package by.babanin.testtask.service;
+package by.babanin.testtask.service.exception;
 
-public class UserExistsException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class UserExistsException extends RuntimeException {
     public UserExistsException() {
     }
 
