2ef2f8a2942f8de52ddd2ee60f5f368b0ea048a0
LordDetson <babanin.dima@gmail.com>
2019-07-05 15:10:39 +0300

    Service classes

--- TestTask/src/main/java/by/babanin/testtask/service/RoomService.java
+++ TestTask/src/main/java/by/babanin/testtask/service/RoomService.java
@@ -1,4 +1,18 @@
 package by.babanin.testtask.service;
 
+import by.babanin.testtask.dao.RoomRepository;
+import by.babanin.testtask.dao.WorkScheduleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoomService {
+    private final RoomRepository roomRepository;
+    private final WorkScheduleRepository workScheduleRepository;
+
+    public RoomService(RoomRepository roomRepository, WorkScheduleRepository workScheduleRepository) {
+        this.roomRepository = roomRepository;
+        this.workScheduleRepository = workScheduleRepository;
+    }
+
+
 }
--- TestTask/src/main/java/by/babanin/testtask/service/UserService.java
+++ TestTask/src/main/java/by/babanin/testtask/service/UserService.java
@@ -1,4 +1,48 @@
 package by.babanin.testtask.service;
 
-public class UserService {
+import by.babanin.testtask.dao.UserRepository;
+import by.babanin.testtask.entity.User;
+import by.babanin.testtask.entity.UserRole;
+import by.babanin.testtask.service.exception.UserExistsException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return getUserByUsername(username);
+    }
+
+    public User addUser(User user) {
+        User userByUsername = getUserByUsername(user.getUsername());
+        if (!Objects.isNull(userByUsername))
+            throw new UserExistsException("Порльзователь с username " + user.getUsername() + " существует");
+        user.setRoles(Collections.singleton(UserRole.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 }
