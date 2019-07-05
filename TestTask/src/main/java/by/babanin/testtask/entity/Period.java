0be349ad00021c3635a8bf4925f2286e9efcee8a
LordDetson <babanin.dima@gmail.com>
2019-07-05 15:11:37 +0300

    Time frame model

--- TestTask/src/main/java/by/babanin/testtask/entity/Period.java
+++ TestTask/src/main/java/by/babanin/testtask/entity/Period.java
@@ -1,4 +1,24 @@
 package by.babanin.testtask.entity;
 
+import java.time.LocalDateTime;
+
 public class Period {
+    private LocalDateTime start;
+    private LocalDateTime finish;
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getFinish() {
+        return finish;
+    }
+
+    public void setFinish(LocalDateTime finish) {
+        this.finish = finish;
+    }
 }
