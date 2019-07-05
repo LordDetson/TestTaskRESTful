f48557c70334f984aa8eff4b12fb2fea9fe825e6
LordDetson <babanin.dima@gmail.com>
2019-07-05 15:13:48 +0300

    The controller provides access to user services

--- TestTask/src/main/java/by/babanin/testtask/controller/UserController.java
+++ TestTask/src/main/java/by/babanin/testtask/controller/UserController.java
@@ -1,4 +1,21 @@
 package by.babanin.testtask.controller;
 
+import by.babanin.testtask.entity.User;
+import by.babanin.testtask.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registration")
+    public User registration(@RequestBody User user) {
+        return userService.addUser(user);
+    }
 }
