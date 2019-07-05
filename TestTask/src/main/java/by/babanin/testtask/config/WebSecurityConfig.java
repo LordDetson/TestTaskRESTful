6b4af9a5d06b92790ce7a3311eed523a73c37345
LordDetson <babanin.dima@gmail.com>
2019-07-05 15:08:09 +0300

    Configuration class Spring Security

--- TestTask/src/main/java/by/babanin/testtask/config/WebSecurityConfig.java
+++ TestTask/src/main/java/by/babanin/testtask/config/WebSecurityConfig.java
@@ -1,4 +1,61 @@
 package by.babanin.testtask.config;
 
-public class WebSecurityConfig {
+import by.babanin.testtask.config.restComponent.MySavedRequestAwareAuthenticationSuccessHandler;
+import by.babanin.testtask.config.restComponent.RestAuthenticationEntryPoint;
+import by.babanin.testtask.service.UserService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler;
+    private final SimpleUrlAuthenticationFailureHandler myFailureHandler;
+
+    public WebSecurityConfig(
+            UserService userService,
+            PasswordEncoder passwordEncoder,
+            RestAuthenticationEntryPoint restAuthenticationEntryPoint,
+            MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.mySuccessHandler = mySuccessHandler;
+        this.myFailureHandler = new SimpleUrlAuthenticationFailureHandler();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/registration").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .successHandler(mySuccessHandler)
+                .failureHandler(myFailureHandler)
+                .and()
+                .logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder.encode("admin")).roles("ADMIN")
+                .and()
+                .withUser("user").password(passwordEncoder.encode("user")).roles("USER");*/
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
 }
