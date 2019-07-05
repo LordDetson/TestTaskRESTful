5cbe1616201121bb1ddd88d17827469e0a36cb72
LordDetson <babanin.dima@gmail.com>
2019-07-05 14:59:11 +0300

    Model with information about a room

---
+++ TestTask/src/main/java/by/babanin/testtask/entity/Room.java
@@ -0,0 +1,60 @@
+package by.babanin.testtask.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Document
+public class Room {
+    @Id
+    private String id;
+    private String number;
+    private Integer amountOfBeds;
+    private Double price;
+    private Map<String, Period> reservation;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Integer getAmountOfBeds() {
+        return amountOfBeds;
+    }
+
+    public void setAmountOfBeds(Integer amountOfBeds) {
+        this.amountOfBeds = amountOfBeds;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Map<String, Period> getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Map<String, Period> reservation) {
+        this.reservation = reservation;
+    }
+
+    public boolean isReservation() {
+        return reservation != null && !reservation.isEmpty();
+    }
+}
