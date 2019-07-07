a1cfe7512bd9a5db57acd600dc5d9adb3fe6b7ef
LordDetson <babanin.dima@gmail.com>
2019-07-07 13:28:18 +0300

    Model with information about amount and days reservation

--- TestTask/src/main/java/by/babanin/testtask/entity/BookingFee.java
+++ TestTask/src/main/java/by/babanin/testtask/entity/BookingFee.java
@@ -1,4 +1,22 @@
 package by.babanin.testtask.entity;
 
 public class BookingFee {
+    private Double amount;
+    private Long days;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getDays() {
+        return days;
+    }
+
+    public void setDays(Long days) {
+        this.days = days;
+    }
 }
