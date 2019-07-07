d55ab0cea10990a153d2cd64b8cabf670d4b9cd2
LordDetson <babanin.dima@gmail.com>
2019-07-07 12:35:53 +0300

    Enable permission checking

--- TestTask/src/main/java/by/babanin/testtask/config/WebSecurityConfig.java
+++ TestTask/src/main/java/by/babanin/testtask/config/WebSecurityConfig.java
@@ -2,26 +2,29 @@ package by.babanin.testtask.config;
 
 import by.babanin.testtask.config.restComponent.MySavedRequestAwareAuthenticationSuccessHandler;
 import by.babanin.testtask.config.restComponent.RestAuthenticationEntryPoint;
-import by.babanin.testtask.service.UserService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
+    private final UserDetailsService userService;
     private final PasswordEncoder passwordEncoder;
     private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
     private final MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler;
     private final SimpleUrlAuthenticationFailureHandler myFailureHandler;
 
     public WebSecurityConfig(
-            UserService userService,
+            @Qualifier("userServiceImpl") UserDetailsService userService,
             PasswordEncoder passwordEncoder,
             RestAuthenticationEntryPoint restAuthenticationEntryPoint,
             MySavedRequestAwareAuthenticationSuccessHandler mySuccessHandler) {
@@ -52,10 +55,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        /*auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder.encode("admin")).roles("ADMIN")
-                .and()
-                .withUser("user").password(passwordEncoder.encode("user")).roles("USER");*/
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
     }
 }
