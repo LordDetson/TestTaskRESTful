dce86f2f3644cd5853190862eadf18ee8b63f620
LordDetson <babanin.dima@gmail.com>
2019-07-07 12:48:19 +0300

    Add method toString()

--- TestTask/src/main/java/by/babanin/testtask/entity/Room.java
+++ TestTask/src/main/java/by/babanin/testtask/entity/Room.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
+import java.util.StringJoiner;
 
 @Document
 public class Room {
@@ -56,5 +57,16 @@ public class Room {
 
     public boolean isReservation() {
         return reservation != null && !reservation.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Room.class.getSimpleName() + "[", "]")
+                .add("id='" + id + "'")
+                .add("number='" + number + "'")
+                .add("amountOfBeds=" + amountOfBeds)
+                .add("price=" + price)
+                .add("reservation=" + reservation)
+                .toString();
     }
 }
