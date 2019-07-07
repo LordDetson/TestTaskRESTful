96d486917c55c8fc4f5d8a28cf65ac65bc38ea09
LordDetson <babanin.dima@gmail.com>
2019-07-07 12:41:34 +0300

    Add methods for find not reserved room and find room by number

--- TestTask/src/main/java/by/babanin/testtask/dao/RoomRepository.java
+++ TestTask/src/main/java/by/babanin/testtask/dao/RoomRepository.java
@@ -3,5 +3,10 @@ package by.babanin.testtask.dao;
 import by.babanin.testtask.entity.Room;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends MongoRepository<Room, String> {
+    List<Room> findRoomsByReservationIsNull();
+
+    Room findByNumber(String number);
 }
